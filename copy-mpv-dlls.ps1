43783e6d73fb1781acd5ad51f2bb78207f65b9c9
Jayson Reis <santosdosreis@gmail.com>
2026-01-03 20:39:27 +0100

    Remove unused wraps and patches

--- copy-mpv-dlls.ps1
+++
@@ -1,7 +0,0 @@
-$Dlls = dir ..\mpv\build -include "*.dll" -recurse
-mkdir .cache
-
-foreach ($Dll in $Dlls) {
-    Copy-Item $Dll.FullName -Destination ".cache/" -Force
-    Write-Host "Copied $($Dll.Name)"
-}
--- scripts/android-write-ndk-root.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-NDK_ROOT="$(grep --color=auto -oE '/.+?clang' "$1" | head -n 1 | sed 's|/toolchains/.*||')"
-if grep CMAKE_ANDROID_NDK "$1"; then
-   exit 0
-fi
-
-printf "\n[cmake]\nCMAKE_ANDROID_NDK = '%s'\nNDK_PROC_aarch64_ABI='arm64-v8a'\nNDK_PROC_x86_64_ABI='x86_64'\nCMAKE_POSITION_INDEPENDENT_CODE='ON'\n" "$NDK_ROOT" >> "$1"
